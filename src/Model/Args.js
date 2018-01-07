17de65dc98b697290de1c58eef0639c762aca19d
Adam Saladino <adam.saladino@ellucian.com>
2018-01-06 20:18:11 -0500

    Added folder check and code comments.

--- src/Model/Args.js
+++ src/Model/Args.js
@@ -2,21 +2,35 @@ const FileDetails = require('./FileDetails');
 const path = require("path");
 const fs = require("fs");
 
+/**
+ * Commandline arguments being passed to the indexer.
+ */
 class Args {
     constructor(params) {
         /**
-         * @type FileDetails
+         * Project directory to output the index results.
+         * @type {FileDetails|null}
          */
         this.output = null;
         /**
-         * @type {string}
+         * Domain being indexed.
+         * @type {string|*}
          */
         this.domain = null;
+        /**
+         * Should progress information be output to the console?
+         * @type {boolean}
+         */
+        this.verbose = true;
         Object.assign(this, params);
     }
 
+    /**
+     * If the mandatory options are not passed then show the menu.
+     * @returns {boolean} true if the mandatory options are not passed.
+     */
     shouldShowHelp() {
-        return this.hasOwnProperty('help') || !this.domain || !this.output;
+        return this.hasOwnProperty('help') || (this.domain === null || this.output === null);
     }
 
     getSiteName() {
